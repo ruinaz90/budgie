d068989de562ce3acf48b5de21bc9ce966bcd190
Ruina Z <rzhao9002@gmail.com>
2022-07-27 06:20:12 -0400

    MongoDB

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+const bodyParser = require('body-parser')
 const app = express()
+const connectionString = 'mongodb+srv://admin:pass@cluster0.1brh5bj.mongodb.net/?retryWrites=true&w=majority'
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true}))
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+MongoClient.connect(connectionString, {
+    useUnifiedTopology: true })
+    .then(client => {
+        console.log("Connected to database")
+        const db = client.db('Budgie')
+        const budgets = db.collection('budgets')
 
-app.listen(3000, function() {
-    console.log("listening on 3000")
-})
+        app.get('/', (req, res) => {
+            res.sendFile(__dirname + '/index.html')
+        })
+
+        app.listen(3000, function() {
+            console.log("listening on 3000")
+        })
+    })
